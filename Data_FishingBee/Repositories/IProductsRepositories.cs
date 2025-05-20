b648e234c57836af155d740a2c010d11dc2d24f8
HungHe198 <levanhung.1982004@gmail.com>
2025-05-20 19:15:33 +0700

    update

---
+++ Data_FishingBee/Repositories/IProductsRepositories.cs
@@ -0,0 +1,14 @@
+﻿using Data_FishingBee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_FishingBee.Repositories
+{
+    public interface IProductsRepositories
+    {
+        public Task<List<Product>> GetAllByCategoryIdAsync(Guid categoryId);
+    }
+}
