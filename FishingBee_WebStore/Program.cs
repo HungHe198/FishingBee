3c65c17efee22b1dd9baf5e883ce9e8a01b67e83
hieu8544 <151865227+hieu8544@users.noreply.github.com>
2025-05-29 14:09:21 +0700

    view29/05

--- FishingBee_WebStore/Program.cs
+++ FishingBee_WebStore/Program.cs
@@ -62,6 +62,10 @@ app.UseSession();
 app.UseAuthentication(); 
 app.UseAuthorization();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.MapControllerRoute(
     name: "search",
